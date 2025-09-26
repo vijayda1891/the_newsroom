2616f0bb4bc0122069797fed346376626ff4ade3
vijayda1891 <vijayda2009@gmail.com>
2025-09-26 14:51:49 +0530

    Finishing all game review related devlopment

--- app/helpers/game_reviews_helper.rb
+++ app/helpers/game_reviews_helper.rb
@@ -1,2 +1,15 @@
 module GameReviewsHelper
+    def ratings_helper(rating)
+        if  rating >= 90
+            return "Masterpiece"
+        elsif rating >= 80  && rating < 90
+            return "Amazing"
+        elsif rating >= 70  && rating < 80
+            return "Great"
+        elsif rating >= 60  && rating < 70
+            return "Good"      
+        else
+            return "Average"
+        end
+    end
 end
